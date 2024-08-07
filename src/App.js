80988fae204fc9270d247437a18adb5dbac581ba
aaron <aaronsoh01@gmail.com>
2024-08-07 12:07:37 +0800

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./views/HomePage";
+import ListingAddPage from "./views/ListingAddPage";
+import ListingEditPage from "./views/ListingEditPage";
+import ListingPage from "./views/ListingPage";
+import LoginPage from "./views/LoginPage";
+import SignUpPage from "./views/SignUpPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/add" element={<ListingAddPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/update/:id" element={<ListingEditPage />} />
+        <Route path="/posts/:id" element={<ListingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
